531d91820893c258b4a08a51accffaf70cf197ae
TAO933 <taow8603@gmail.com>
2026-03-20 11:54:02 +0800

    Update and rename 10-score.js to 01-score.js

---
+++ homework/01-score.js
@@ -0,0 +1,9 @@
+function checkOddEven(num) {
+  if (num % 2 === 0) {
+    console.log(`${num} 是偶數`);
+  } else {
+    console.log(`${num} 是奇數`);
+  }
+}
+checkOddEven(7);  // 7 是奇數
+checkOddEven(12); // 12 是偶數
